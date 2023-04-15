64572492268502e813743a01320d376a104e98dd
David Andrade <david.gr.andrade@gmail.com>
2023-04-14 22:50:39 -0400

    FInished code for TODO list code lab

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,29 +1,113 @@
 import React from "react";
 import "./App.css";
+
+
 const App = () => {
   const [todos, setTodos] = React.useState([]);
   const [todo, setTodo] = React.useState("");
   
+  //temp constants for edit function
+    const [todoEditing, setTodoEditing] = React.useState(null);
+    const [editingText, setEditingText] = React.useState("");
   // Add the handlesubmit code here
-  
+  function handleSubmit(e){
+      e.preventDefault();
+
+      const newTodo = {
+          id: new Date().getTime(),
+          text: todo.trim(),
+          completed: false,
+      };
+
+      if(newTodo.text.length >0){
+          setTodos([...todos].concat(newTodo));
+          setTodo("");
+      }else{
+          alert("Enter a valid task here");
+          setTodo("");
+      }
+  }
   
   // Add the deleteToDo code here
-
+    function deleteTodo(id){
+        let updatedTodos = [...todos].filter( (todo) => todo.id !== id);
+        setTodos(updatedTodos);
+    }
   
-  // Add the toggleComplete code here
+    // Add the toggleComplete code here
+    function toggleComplete(id){
+        let updatedTodos = [...todos].filter( (todo) =>{
+            if (todo.id === id){
+                todo.completed = !todo.completed;
+            }
 
+            return todo;
+        });
+
+        setTodos(updatedTodos);
+    }
   
   // Add the submitEdits code here
+  function submitEdits(id) {
+    const updatedTodos = [...todos].map((todo) => {
+      if (todo.id === id) {
+        todo.text = editingText;
+        }
+        return todo;
+      });
+      setTodos(updatedTodos);
+      setTodoEditing(null);
+    }
 
-  
+    //Added hooks
+    React.useEffect( () => {
+        const json = localStorage.getItem("todos");
+        const loadedTodos = JSON.parse(json);
+
+        if(loadedTodos){
+            setTodos(loadedTodos);
+        }
+    },[]);
+
+    React.useEffect( () => {
+        if([todos].length >0){
+            const json = JSON.stringify(todos);
+            localStorage.setItem("todos",json);
+        }
+    },[todos]); 
 return(
 <div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" />
-<button type ="submit">Add Todo</button>
-</form>
+    <h1>Todo List</h1>
+    <form onSubmit={handleSubmit}>
+    <input type ="text" align ="right" 
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Add a new Task"
+        value={todo}/>
+    <button type ="submit">Add Todo</button>
+    </form>
+        {todos.map( (todo) => <div>
+                <div>
+                <input type="checkbox" id="completed" 
+                    checked={todo.completed} 
+                    onChange={() => toggleComplete(todo.id)}/>
+                {todo.id === todoEditing ? (
+                <input type="text" onChange={(e) => setEditingText(e.target.value)} />
+                ) : (
+                <div>{todo.text}</div>
+                )}
+                </div>
+            <div className="todo-actions">
+            {todo.id === todoEditing ? (
+            <button onClick={() => submitEdits(todo.id)}>Submit Edits</button>
+            ) : (
+            <button onClick={() => setTodoEditing(todo.id)}>Edit</button>
+            )}
+            </div>
+
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        </div>)}
 </div>
 );
+
 };
 export default App;
